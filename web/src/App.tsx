412f18d61387142242a2f3d3003e5913c4b4bdca
Lucas da Silva Leoncio <47578616+lucasdksan@users.noreply.github.com>
2020-07-31 12:39:11 -0300

    Add files via upload

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//const React = require('react');
+import './App.css';
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
